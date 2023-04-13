57dc4b734b73d09f2cf5a8420b760e1a79a03657
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-04-12 23:30:06 -0300

    Feat: First unit test

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,17 +1,26 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
-interface IRegisterUseCase {
+interface IRegisterUseCaseRequest {
   name: string
   email: string
   password: string
 }
 
+interface IRegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: IRegisterUseCase) {
+  async execute({
+    name,
+    email,
+    password,
+  }: IRegisterUseCaseRequest): Promise<IRegisterUseCaseResponse> {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
@@ -20,10 +29,14 @@ export class RegisterUseCase {
 
     const password_hash = await hash(password, 6)
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
